d267617c3be0fdb4c4e1b521eb70f3ae7587e1fb
JohnGrisham <jgrisham@vhtcx.com>
2023-03-06 14:18:46 -0600

    Added openapi package

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import Head from 'next/head';
 import { Button } from 'ui';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,20 +10,20 @@ export default function Home() {
   console.log({ response });
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Web - Turborepo Example</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="w-auto px-4 pt-16 pb-8 mx-auto sm:pt-24 lg:px-8">
+      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <h1 className="mx-auto max-w-5xl text-center text-6xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-7xl lg:text-8xl xl:text-8xl">
           Web <br className="hidden lg:block" />
-          <span className="inline-block text-transparent bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text">
+          <span className="inline-block bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent">
             Turborepo Example
           </span>{' '}
         </h1>
-        <div className="max-w-xl mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
+        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
           <Button>
             <a href="https://turborepo.org/docs/getting-started">
               Read the docs <FontAwesomeIcon icon={faArrowRight} size="sm" />
