357d6e81bc871c257ffcd324c14ae192a059c9ab
JohnGrisham <jgrisham@vhtcx.com>
2023-05-11 08:55:47 -0500

    Fixed styling issue on sign in page, wored on getting dev command to run all the things

--- packages/ui/src/components/forms/signin/credentialsSignin.tsx
+++ packages/ui/src/components/forms/signin/credentialsSignin.tsx
@@ -7,20 +7,18 @@ import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 interface CredentialsSignupProps {
   backgroundImage: React.ReactNode;
-  children?: React.ReactNode;
   signupUrl?: string;
   signInOptions?: SignInOptions;
 }
 
 export const CredentialsSignin: React.FC<CredentialsSignupProps> = ({
   backgroundImage,
-  children,
   signupUrl,
   signInOptions,
 }) => (
   <section className="w-screen">
-    <div className="flex flex-wrap items-center justify-center text-gray-800 g-6">
-      <div className="w-7/12 -mt-10 md:mt-0 lg:w-5/12">{backgroundImage}</div>
+    <div className="g-6 flex flex-wrap items-center justify-center text-gray-800">
+      <div className="-mt-10 w-7/12 md:mt-0 lg:w-5/12">{backgroundImage}</div>
       <div className="lg:w-4/12">
         <Form
           initialValues={{
@@ -49,15 +47,15 @@ export const CredentialsSignin: React.FC<CredentialsSignupProps> = ({
                 type="password"
                 required
               />
-              <div className="flex items-center justify-between mb-6">
+              <div className="mb-6 flex items-center justify-between">
                 <div className="form-group form-check">
                   <input
                     type="checkbox"
-                    className="float-left w-4 h-4 mt-1 mr-2 text-white align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border border-gray-300 rounded-sm appearance-none cursor-pointer form-check-input checked:border-primary-600 checked:bg-primary-600 focus:outline-none"
+                    className="form-check-input float-left mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top text-white transition duration-200 checked:border-primary-600 checked:bg-primary-600 focus:outline-none"
                     id="remember-me"
                   />
                   <label
-                    className="inline-block form-check-label text-primary-600"
+                    className="form-check-label inline-block text-primary-600"
                     htmlFor="remember-me"
                   >
                     Remember me
@@ -65,7 +63,7 @@ export const CredentialsSignin: React.FC<CredentialsSignupProps> = ({
                 </div>
                 <a
                   href="#!"
-                  className="transition duration-200 ease-in-out text-primary-600 hover:text-primary-700 focus:text-primary-700 active:text-primary-800"
+                  className="text-primary-600 transition duration-200 ease-in-out hover:text-primary-700 focus:text-primary-700 active:text-primary-800"
                 >
                   Forgot password?
                 </a>
@@ -73,13 +71,13 @@ export const CredentialsSignin: React.FC<CredentialsSignupProps> = ({
               <Button
                 disabled={!isValid}
                 type="submit"
-                className="inline-block w-full py-3 text-sm font-medium leading-snug text-white uppercase transition duration-150 ease-in-out rounded shadow-md bg-primary-600 px-7 hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+                className="inline-block w-full rounded bg-primary-600 py-3 px-7 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
               >
                 Sign in
               </Button>
-              <div className="flex flex-col items-center text-xs uppercase font-small px-7">
+              <div className="font-small flex flex-col items-center px-7 text-xs uppercase">
                 {signupUrl && (
                   <a
                     className="mt-2 text-primary-600 hover:cursor-pointer dark:text-white"
@@ -90,12 +88,12 @@ export const CredentialsSignin: React.FC<CredentialsSignupProps> = ({
                 )}
               </div>
               <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-gray-300 after:mt-0.5 after:flex-1 after:border-t after:border-gray-300">
-                <p className="mx-4 mb-0 font-semibold text-center text-primary-600">
+                <p className="mx-4 mb-0 text-center font-semibold text-primary-600">
                   OR
                 </p>
               </div>
               <a
-                className="flex items-center justify-center w-full py-3 mb-3 text-sm font-medium leading-snug text-white uppercase transition duration-150 ease-in-out rounded shadow-md px-7 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+                className="mb-3 flex w-full items-center justify-center rounded py-3 px-7 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
                 style={{ backgroundColor: '#0F9D58' }}
                 href="#!"
                 role="button"
@@ -106,7 +104,6 @@ export const CredentialsSignin: React.FC<CredentialsSignupProps> = ({
                 <FontAwesomeIcon icon={faGoogle} />
                 &nbsp; Continue with Google
               </a>
-              {children}
             </>
           )}
         </Form>
