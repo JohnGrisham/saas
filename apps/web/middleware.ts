360c22151b0defb2cc00af4071f420916568bed0
JohnGrisham <jgrisham@vhtcx.com>
2023-08-28 14:53:50 -0500

    Turbo config improvements

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,6 +3,6 @@ import { middleware as platformMiddleware } from 'platforms';
 const multidomain: boolean = JSON.parse(
   process.env.NEXT_PUBLIC_MULTIDOMAIN ?? 'false',
 );
-const middleware = multidomain ? platformMiddleware : () => {};
+const middleware = multidomain ? platformMiddleware : async () => {};
 
 export default middleware;
